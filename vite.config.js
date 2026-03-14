33779529767248f9519742caaf708d0a96d3112e
shafikulnayem10 <inayem98@gmail.com>
2026-03-15 01:14:38 +0600

    fixed deploy related issues

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Country_React_App/', // 🚨 Must match your GitHub repo name
+  base: '/Country-Tracker/',  
   plugins: [react()],
 })
 
